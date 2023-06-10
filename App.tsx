30e8c33821b023bbc221ed2a6d0e1328fc54a3d5
Felipe Miranda Marreiros <91689754+felipe-miranda-marreiros@users.noreply.github.com>
2023-06-09 22:50:15 -0300

    feat: add button component

--- App.tsx
+++ App.tsx
@@ -1,27 +1,26 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {SafeAreaView} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 import {Text} from './src/components/Text/Text';
+import {Button} from './src/components/Button/Button';
+import {ThemeProvider} from '@shopify/restyle';
+import {theme} from './src/theme/theme';
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView>
-      <Text preset="headingLarge">Felipe</Text>
-    </SafeAreaView>
+    <ThemeProvider theme={theme}>
+      <SafeAreaView style={{paddingHorizontal: 20}}>
+        <Text preset="headingLarge">Felipe</Text>
+        <Button title="Entrar" marginTop="s24" isLoading disabled />
+        <Button
+          title="Outline"
+          disabled
+          preset="outline"
+          isLoading
+          marginTop="s24"
+        />
+      </SafeAreaView>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Box/Box.tsx
@@ -0,0 +1,37 @@
+import {
+  TouchableOpacity,
+  TouchableOpacityProps as RNTouchableOpacityProps,
+} from 'react-native';
+import {
+  backgroundColor,
+  border,
+  createBox,
+  createRestyleComponent,
+  layout,
+  spacing,
+  BackgroundColorProps,
+  SpacingProps,
+  LayoutProps,
+  BorderProps,
+  spacingShorthand,
+  SpacingShorthandProps,
+} from '@shopify/restyle';
+import {Theme} from '../../theme/theme';
+
+export const Box = createBox<Theme>();
+export type BoxProps = React.ComponentProps<typeof Box>;
+
+export type TouchableOpacityBoxProps = BackgroundColorProps<Theme> &
+  SpacingProps<Theme> &
+  BorderProps<Theme> &
+  LayoutProps<Theme> &
+  SpacingShorthandProps<Theme> &
+  RNTouchableOpacityProps;
+
+export const TouchableOpacityBox = createRestyleComponent<
+  TouchableOpacityBoxProps,
+  Theme
+>(
+  [backgroundColor, spacing, spacingShorthand, layout, border],
+  TouchableOpacity,
+);
