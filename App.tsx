339f96a979c913db89c98a7122e3fda89ec3b460
Felipe Miranda Marreiros <91689754+felipe-miranda-marreiros@users.noreply.github.com>
2023-06-12 20:46:58 -0300

    feat: add icons

--- App.tsx
+++ App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
 
-import {Text} from './src/components/Text/Text';
-import {Button} from './src/components/Button/Button';
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
+import {EyeOffIcon} from './src/assets/icons/EyeOffIcon';
+import {EyeOnIcon} from './src/assets/icons/EyeOnIcon';
+import {Icon} from './src/components/Icon/Icon';
 
 function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView style={{paddingHorizontal: 20}}>
-        <Text preset="headingLarge">Felipe</Text>
-        <Button title="Entrar" marginTop="s24" isLoading disabled />
-        <Button
-          title="Outline"
-          disabled
-          preset="outline"
-          isLoading
-          marginTop="s24"
-        />
+        <EyeOffIcon />
+        <EyeOnIcon />
+        <Icon name="eyeOff" />
       </SafeAreaView>
     </ThemeProvider>
   );
